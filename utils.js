1373b12d2e9c1ba3cfc65de6316dd344e9fde643
Ara Boghozian <araboghozian@Aras-MacBook-Air.local>
2017-03-05 21:10:13 -0800

    1st commit

---
+++ utils.js
@@ -0,0 +1,36 @@
+function sum(a, b) {
+  return a + b;
+}
+
+export function average(a, b) {
+  return sum(a, b) / 2;
+}
+
+export const MAX = 1000;
+
+export default function max(a, b) {
+  return a > b ? a : b;
+}
+
+export function measure(a) {
+  return a.filter(n => n >= 10);
+}
+
+export function add(a) {
+  return a.map((n => n + 10));
+}
+
+
+export function notequal(a, b) {
+  let ar;
+  for (let d = 0; d < b.length; d++) {
+    ar.push(a.filter(n => n !== b[d]));
+  }
+  return ar;
+}
+
+export function dbl(a) {
+  let b;
+  b.push(a.filter(n => n > 20));
+  return b.map(e => e * 2);
+}
